0375e4d3d17f8d1f2a7a943393e5f7b154c1e3c5
Firred <adri-ankenobi@hotmail.com>
2020-06-06 15:59:42 +0200

    Service Worker funcionando.

--- javascript/serviceWorkerRegister.js
+++ javascript/serviceWorkerRegister.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         //Registra y da feedback dependiendo del resultado
-        navigator.serviceWorker.register('/javascript/sw.js').then(reg => {
+        navigator.serviceWorker.register('./sw.js').then(reg => {
             console.log('El navegador ejecuta service workers ', reg)
         }, function (err) {
             console.log('Fallo. El navegador no soporta service workers ', err)
